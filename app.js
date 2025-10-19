304125fa082f157c40658937212312bc1c95966d
nguyenhuuduc64 <duchuuiq@gmail.com>
2025-10-19 19:09:09 +0700

    Dinh nghia cac routes cho tai nguyen contact

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
 
 app.use(cors());
 app.use(express.json());
