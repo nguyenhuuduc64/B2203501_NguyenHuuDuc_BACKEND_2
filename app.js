a0330d8f779525745e542bc45e2a439bf06e758f
nguyenhuuduc64 <duchuuiq@gmail.com>
2025-10-19 19:11:57 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.use("/api/contacts", contactsRouter);
 app.use(cors());
 app.use(express.json());
 
+const ApiError = require("./app/api-error");
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
